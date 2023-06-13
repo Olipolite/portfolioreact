81e60a359ffd57ea25fff1b730cca073aad37366
Olipolite <oliver.oquist3d@gmail.com>
2023-06-13 21:18:16 +0100

    Made the navbar and the hero-text-image much more responsive. Pretty good all sizes.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,17 +3,16 @@ import Oliver from '../nocactus.png';
 
 function Home() {
   return (
-    <div className="container flex-col justify-center items-center mx-auto grid grid-cols-6 px-4 sm:px-6 lg:px-8">
-      <div className="md:col-span-1" />
-      <div className=" md:flex items-center">
-        <div className="col-span-4 mr-4">
-          <p className="text-left">
-            Im a software developer who loves creating stuff and solve things
+    <div className="container flex-col justify-center items-center mx-auto">
+      <div className="grid-cols-5 gap-10 lg:gap-20 pl-4 md:flex items-center md:px-32 xl:px-64">
+        <div className="col-span-4">
+          <h1 className="text-5xl font-bold text-white py-8">{"Yoo, I'm Oliver!!"}</h1>
+          <p className="text-white w-full xl:text-xl lg:w-full">
+            {"I'm a software developer who loves creating stuff and solve things. If I'm not coding i'm propably outside playing volleyball/tennis or beating someone up in Street Fighter."}
           </p>
         </div>
-        <img src={Oliver} className="w-64 h-auto col-span-1" alt="Pic of me" />
+        <img src={Oliver} className="w-64 h-auto col-span-1 sm:pt-28" alt="Pic of me" />
       </div>
-      <div className="md:col-span-1" />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,10 +32,10 @@ function Navbar() {
 
   return (
     <div className="bg-black text-white">
-      <div className=" container mx-auto grid grid-cols-6 px-4 sm:px-6 lg:px-8">
-        <div className="lg:col-span-1" />
-        <div className="flex items-center justify-between h-16 col-span-4">
-          <Link className="flex  text-white text-sm md:text-xl" to="/">奧 Oliver Öquist</Link>
+      <div className="container mx-auto px-10 md:px-32 lg:px-64">
+        <div className="" />
+        <div className="flex items-center justify-between h-16">
+          <Link className="flex text-white text-xl" to="/">奧 Oliver Öquist</Link>
           {/* nav */}
           <div className="hidden md:block">
             <div className="ml-10 flex items-baseline space-x-4">
@@ -70,7 +70,7 @@ function Navbar() {
             </div>
           ) : null}
         </div>
-        <div className="lg:col-span-1" />
+        <div className="" />
       </div>
     </div>
   );
