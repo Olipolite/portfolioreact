958b0f53100d03b3e2a1c9623a45cd80612930f8
Olipolite <oliver.oquist3d@gmail.com>
2023-07-17 16:07:45 +0100

    Image and text properly aligning now

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,8 +4,8 @@ import Oli from '../IMG_3544.JPG';
 function About() {
   return (
     <div id="about" className="container mx-auto flex-grow min-h-screen pt-20">
-      <div className="flex flex-col items-center text-center sm:text-left sm:px-4 lg:flex-row lg:items-center md:px-32 lg:px-40 xl:px-64">
-        <img src={Oli} alt="oliver" className="w-72 h-auto lg:self-start lg:h-[550px] lg:w-[800px]" />
+      <div className="flex flex-col items-center text-center sm:text-left sm:px-4 lg:flex-row lg:items-start  md:px-32 lg:px-40 xl:px-64">
+        <img src={Oli} alt="oliver" className="w-72 h-auto lg:h-[550px] lg:w-[800px]" />
         <article className="px-5">
           <p className="text-cactus text-[12px] pt-10 lg:pt-0">WHO IS THIS GUY?</p>
           <h3 className="text-3xl text-white pb-4">OLIVER ÖQUIST</h3>
