ee085c1d116bda844c7f2983895d3040539515ce
Olipolite <oliver.oquist3d@gmail.com>
2023-07-17 15:50:05 +0100

    About section picture now centered on mobile devices, and aligned with with text on larger devices

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,14 +3,14 @@ import Oli from '../IMG_3544.JPG';
 
 function About() {
   return (
-    <div id="about" className="container flex-col justify-center items-center mx-auto flex-grow min-h-screen pt-20">
-      <div className=" text-center sm:text-left sm:px-4 lg:flex justify-center md:px-32 lg:px-40 xl:px-64">
-        <img src={Oli} alt="oliver" className="w-72 h-auto lg:h-[550px] lg:w-[800px] pr-5 overflow-visible" />
-        <article className="pl-5">
+    <div id="about" className="container mx-auto flex-grow min-h-screen pt-20">
+      <div className="flex flex-col items-center text-center sm:text-left sm:px-4 lg:flex-row lg:items-center md:px-32 lg:px-40 xl:px-64">
+        <img src={Oli} alt="oliver" className="w-72 h-auto lg:self-start lg:h-[550px] lg:w-[800px]" />
+        <article className="px-5">
           <p className="text-cactus text-[12px] pt-10 lg:pt-0">WHO IS THIS GUY?</p>
           <h3 className="text-3xl text-white pb-4">OLIVER ÖQUIST</h3>
           <div>
-            <p className="text-gray-300 whitespace-pre-line">
+            <p className="text-left text-gray-300 whitespace-pre-line">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Non velit nesciunt molestiae voluptates, iure cupiditate,
               molestias doloremque vel consequuntur
